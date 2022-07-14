89cac3b17a47220e49670a5f4cbf20451a62b849
Andrii Martyniuk <martynandre@gmail.com>
2022-07-14 11:38:27 +0300

    linter7

--- api.py
+++ api.py
@@ -1,6 +1,4 @@
-from asyncio.proactor_events import _ProactorBasePipeTransport
 import os
-from urllib import response
 
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
